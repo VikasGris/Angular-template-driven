368e69dd1c058f98c57a51bea0df144fcf34df8d
VikasGris <vikasgowri0913@gmail.com>
2021-04-13 17:56:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'form-handle';
+  defaultEmail = "vikasponnusamy019@gmail.com"
+  answer = '';
+  genders = ["Male", "Female"];
+  user = {
+    username: '',
+    email: '',
+    gender: '',
+    
+  };
+  submitted = false;
+
+  @ViewChild('f')
+  signupForm!: NgForm;
+
+  suggestName() {
+    const name = "Appa"
+    this.signupForm.form.patchValue({
+      userData: {
+        username: name,
+        email:"Vik@gamil.com"
+      },
+      secret: 'pet',
+      
+    })
+  }
+  
+  onSubmit() {
+    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
+  }
+  
+
+  // onSubmit(form:NgForm) {
+  //   console.log(form);
+  // }
+
+  
 }
